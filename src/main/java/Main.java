2ade7eaec0b8eff30732bc6955819f92a808a487
BuildTools <erdieroni@gmail.com>
2023-06-17 11:45:51 +0200

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.erdi.hl3richpresence.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        new Thread(new Application()).start();
+    }
+}
---
+++ src/main/java/com/erdi/hl3richpresence/Application.java
@@ -0,0 +1,50 @@
+package com.erdi.hl3richpresence;
+
+import net.arikia.dev.drpc.DiscordEventHandlers;
+import net.arikia.dev.drpc.DiscordRPC;
+import net.arikia.dev.drpc.DiscordRichPresence;
+import net.arikia.dev.drpc.DiscordUser;
+
+import javax.swing.*;
+import java.util.concurrent.TimeUnit;
+
+public class Application implements Runnable {
+    public static final String APP_ID = "1119552096426020934";
+
+    private DiscordEventHandlers eventHandlers;
+
+    private void setupHandlers() {
+        eventHandlers = new DiscordEventHandlers.Builder().setReadyEventHandler(user -> {
+            DiscordRichPresence richPresence = new DiscordRichPresence.Builder("Multiplayer")
+                    .setDetails("Episode 3")
+                    .setParty("", 3, 3)
+                    .setSecrets("", "")
+                    .setSmallImage("hl3", "Half Life 3 Episode 3")
+                    .setBigImage("hl3", "Half Life 3 Episode 3")
+                    .setStartTimestamps(System.currentTimeMillis())
+                    .setEndTimestamp(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(1))
+                    .build();
+            DiscordRPC.discordUpdatePresence(richPresence);
+        }).build();
+    }
+
+    @Override
+    public void run() {
+        setupHandlers();
+        DiscordRPC.discordInitialize(APP_ID, eventHandlers, false);
+        DiscordRPC.discordRegister(APP_ID, "");
+
+        JFrame frame = new JFrame("HL3");
+        frame.setSize(50, 50);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Closing Discord hook.");
+            DiscordRPC.discordShutdown();
+        }));
+
+        while(true)
+            DiscordRPC.discordRunCallbacks();
+    }
+}
